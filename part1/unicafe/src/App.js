ae4c2527310172e162e727546310a6c8f8e44b09
yueshengw <78110664+yueshengw@users.noreply.github.com>
2022-07-29 17:27:53 -0400

    Exercise 1.8 Complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,6 +13,23 @@ const Display = ({ text, value, symbol }) => {
   )
 }
 
+const Statistics = ({ count, data }) => {
+  const info = 
+      <>
+      <Display text='good' value={data.good} />
+      <Display text='neutral' value={data.neutral} />
+      <Display text='bad' value={data.bad} />
+      <Display text='all' value={data.all} />
+      <Display text='average' value={data.average} />
+      <Display text='positive' value={data.positive} symbol='%'/>
+      </>
+  return (
+    <>
+      {count>0?info:<div>No feedback given</div>}
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -20,22 +37,36 @@ const App = () => {
   const [all, setAll] = useState(0)
   const [average, setAverage] = useState(0)
   const [positive, setPositive] = useState(null)
-
+  const [data, setData] = useState({})
   const voteGood = () => setGood(good + 1)
   const voteNeutral = () => setNeutral(neutral + 1)
   const voteBad = () => setBad(bad + 1)
 
   useEffect(() => setAll(good+neutral+bad), [good, neutral, bad])
+
   useEffect(() => {
     if (good + bad > 0) {
       setAverage((good + bad*-1)/(good+neutral+bad))
     }
   }, [good, neutral, bad])
+
   useEffect(() => {
     if (good + neutral + bad > 0) {
       setPositive(good / (good + neutral + bad) * 100)
     }
   }, [good, neutral, bad])
+
+  useEffect(() => {
+    setData({
+      'good':good,
+      'neutral':neutral,
+      'bad':bad, 
+      'all':all,
+      'average':average,
+      'positive':positive
+    })
+  },[good, neutral, bad])
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -43,12 +74,7 @@ const App = () => {
       <Button onClick={voteNeutral} text='neutral'/>
       <Button onClick={voteBad} text='bad'/>
       <h1>statistics</h1>
-      <Display text='good' value={good} />
-      <Display text='neutral' value={neutral} />
-      <Display text='bad' value={bad} />
-      <Display text='all' value={all} />
-      <Display text='average' value={average} />
-      <Display text='positive' value={positive} symbol='%'/>
+      <Statistics count={all} data={data}/>
     </div>
   )
 }
