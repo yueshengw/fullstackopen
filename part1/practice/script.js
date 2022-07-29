ba94b0deb3e396990524bc5a05fe501886a77752
yueshengw <78110664+yueshengw@users.noreply.github.com>
2022-07-29 12:10:03 -0400

    Name Change For Exercise 1.5 Solution

--- part1/practice/script.js
+++ part1/practice/script.js
@@ -57,3 +57,12 @@ const expression = function yo2(name) {
 }
 
 expression('hi')
+
+const arto = {
+    name: 'Arto Hellas',
+    greet: function() {
+      console.log('hello, my name is ' + this.name)
+    },
+  }
+  
+  setTimeout(arto.greet, 1000)
