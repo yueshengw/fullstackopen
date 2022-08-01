85fb107286150dd8b3870b108acd33431e096c50
yueshengw <78110664+yueshengw@users.noreply.github.com>
2022-08-01 12:34:04 -0400

    Part 2b Lesson Complete

--- part2/part2-notes-part2-1/src/App.js
+++ part2/part2-notes-part2-1/src/App.js
@@ -1,8 +1,9 @@
 import Note from './components/Note'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
   
@@ -23,6 +24,18 @@ const App = (props) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  
+  console.log('render', notes.length, 'notes')
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
   return (
