0d775715c24beed8b05b57fcd106952105cfbfd4
yueshengw <78110664+yueshengw@users.noreply.github.com>
2022-08-01 16:39:42 -0400

    Exercise 2.13 Complete

--- part2/findcountries/src/App.js
+++ part2/findcountries/src/App.js
@@ -12,35 +12,54 @@ const SearchForm = ({ input, onSubmit, onChange, data }) => {
   )
 }
 
+const Country = ({ country, show }) => {
+  const [shouldShow, setShouldShow] = useState(show);
+  //console.log(findCountryInfo(output[0].key));
+  let languages = []
+
+  for (const key in country.languages) {
+    languages.push(country.languages[key])
+  }
+  console.log(shouldShow, country)
+  const toggleShouldShow = () => {
+    console.log('pressed')
+
+    if (shouldShow) {
+      setShouldShow(false);
+    }
+    else {
+      setShouldShow(true);
+    }
+  }
+
+  return (
+    <>
+      <h1>{country.name.official}</h1>
+      {<button onClick={() => toggleShouldShow()}>{shouldShow === false ? <>show</> : <>collapse</>}</button>}
+      {shouldShow &&
+        <>
+          <h2>Capital: {country.capital}</h2>
+          <h2>Area Code: {country.area}</h2>
+          <h2>Continent: {country.continents}</h2>
+          {languages.length === 1 ? <h2>Language </h2> : <h2>Languages</h2>}
+          <ul>{languages.map(language => <li key={language}><h3>{language}</h3></li>)}</ul>
+          <img src={country.flags.png} /></>}
+    </>
+  )
+}
+
 const DisplayOutput = ({ data, filter }) => {
   let output = data.map(country => {
     return filter === '' ? <div key={country.name.official}>{country.name.official}</div> : country.name.official.toLowerCase().includes(filter.toLowerCase()) && <div key={country.name.official}>{country.name.official}</div>
   }).filter(output => output !== false);
-  //this is for name that is sure to exist
+
+  //this is to find the country object
   const findCountryInfo = (officialName) => {
     let tempData = [...data]
     console.log(officialName)
     return tempData.filter(country => country.name.official === officialName)
   }
 
-  const onlyOne = (country) => {
-    //console.log(findCountryInfo(output[0].key));
-    let languages = []
-    for (const key in country.languages) {
-      languages.push(country.languages[key])
-    }
-    return (
-      <>
-        <h1>{country.name.official}</h1>
-        <h2>Capital: {country.capital[0]}</h2>
-        <h2>Area Code: {country.area}</h2>
-        <h2>Continent: {country.continents}</h2>
-        {languages.length === 1 ? <h2>Language </h2> : <h2>Languages</h2>}
-        <ul>{languages.map(language => <li key={language}><h3>{language}</h3></li>)}</ul>
-        <img src={country.flags.png} />
-      </>
-    )
-  }
   let displayOutput = filter === ''
     ?
     <div>Enter letters to start</div> : output.length > 10
@@ -51,9 +70,13 @@ const DisplayOutput = ({ data, filter }) => {
         ?
         <div>No match</div> :
         output.length === 1 ?
-          onlyOne(findCountryInfo(output[0].key)[0])
+          <Country country={findCountryInfo(output[0].key)[0]} show={true} />
           :
-          output;
+          output.map((country) => {
+            let countryObject = findCountryInfo(country.key)[0];
+            console.log(countryObject)
+            return <Country country={countryObject} show={false} key={countryObject.name.official} />
+          })
 
   return (
     <div>
