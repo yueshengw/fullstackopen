8c6eeeee5685d6b1bfda0dbc92263b166bb23d5a
yueshengw <78110664+yueshengw@users.noreply.github.com>
2022-08-04 13:08:47 -0400

    Part 2d Lesson Complete

--- part2/part2-notes-part2-1/src/App.js
+++ part2/part2-notes-part2-1/src/App.js
@@ -1,12 +1,14 @@
 import Note from './components/Note'
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
   
   const addNote = (event) => {
     event.preventDefault()
@@ -69,9 +71,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnNotes))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -82,6 +87,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -96,6 +102,7 @@ const App = () => {
           <input value={newNote} onChange={handleNoteChange}/>
           <button type='submit'>save</button>
       </form>
+      <Footer />
     </div>
   )
 }
