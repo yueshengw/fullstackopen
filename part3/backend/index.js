06c5f19148df637e60964b2107c01167dc3190ad
yueshengw <78110664+yueshengw@users.noreply.github.com>
2022-08-05 12:45:14 -0400

    Part 3a update
    Part 3a lesson complete and setup for exercise.

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,7 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
-
+app.use(express.json())
 let notes = [
     {
       id: 1,
@@ -29,5 +30,59 @@ let notes = [
     res.json(notes)
   })
 
+  app.get('/api/notes/:id',(req,res) => {
+    const id = Number(req.params.id)
+    console.log(id)
+    const note = notes.find((note) => {
+      console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+      return note.id === id
+    })
+    console.log(note)
+    if (note) {
+      res.json(note)
+    }
+    else {
+      res.status(404).send("id doesn't match any from the database");
+    }
+  })
+
+  app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+    res.status(204).end()
+  })
+  // app.delete('/api/notes/:id', (request, response) => {
+  //   const id = Number(request.params.id)
+  //   notes = notes.filter(note => note.id !== id)
+  
+  //   response.status(204).end()
+  // })
+  app.post('/api/notes', (req,res) => {
+    const note = req.body
+    console.log(note)
+    res.json(note)
+  })
+
+  const generateId = () => {
+    const maxId = note.length > 0 ? Math.max(...notes.map(n => n.id)): 0
+    return maxId + 1
+  }
+
+  app.post('/api/notes', (req, res) => {
+    const body = req.body
+    if (!body.content) {
+      return res.status(400).json({
+        error: "content missing"
+      })
+    }
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId()
+    }
+    notes = notes.concat(note)
+    res.json(note)
+  })
 const PORT = 3001
 app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`)})
