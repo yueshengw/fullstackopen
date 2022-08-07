dfe960f5971470da5ff13cd1cb0078a66108a894
yueshengw <78110664+yueshengw@users.noreply.github.com>
2022-08-07 11:09:50 -0400

    Making A Copy Of Notes

---
+++ part3/notes/src/App.js
@@ -0,0 +1,110 @@
+import Note from './components/Note'
+import { useState, useEffect } from 'react'
+import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+
+    // axios
+    // .post('http://localhost:3001/notes', noteObject)
+    // .then(response => {
+    //   setNotes(notes.concat(noteObject))
+    //   setNewNote('')
+    // })
+    noteService
+    .create(noteObject)
+    .then(returnNotes => {
+      setNotes(notes.concat(returnNotes))
+      setNewNote('')
+    })
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  // useEffect(() => {
+  //   console.log('effect')
+  //   axios
+  //     .get('http://localhost:3001/notes')
+  //     .then(response => {
+  //       console.log('promise fulfilled')
+  //       setNotes(response.data)
+  //     })
+  // }, [])
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    // axios.put(url, changedNote).then(response => {
+    //   setNotes(notes.map(note => note.id !== id ? note : response.data))
+    // })
+    noteService
+      .update(id, changedNote)
+      .then(returnNotes => {
+        setNotes(notes.map(note => note.id !== id ? note : returnNotes))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  console.log('render', notes.length, 'notes')
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage}/>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+          <input value={newNote} onChange={handleNoteChange}/>
+          <button type='submit'>save</button>
+      </form>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
