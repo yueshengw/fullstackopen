efc277c745cdbda3ed073205e113dc64a7c9c4f8
yueshengw <78110664+yueshengw@users.noreply.github.com>
2022-08-20 11:36:30 -0400

    Setup mongoose database

--- part4/blogs-backend/index.js
+++ part4/blogs-backend/index.js
@@ -1,43 +1,50 @@
 const http = require('http')
 const express = require('express')
 const app = express()
-const cors = require()
+const cors = require('cors')
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String, 
-    likes: Number
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const mongoUrl = 'mongodb+srv://admin:GQ4kWIXrtKAtlOkM@cluster0.iwtw4mn.mongodb.net/blogApp?retryWrites=true&w=majority'
+mongoose
+    .connect(mongoUrl)
+    .then(() => {
+        console.log('mongoose connected successfully')
+    })
+    .catch(() => {
+        console.log('failed to connect to mongoose')
+    })
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (req, res) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            res.json(blogs)
-        })
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
 })
 
-app.post('/api/blogs', (req, res) => {
-    const blog = new Blog(req.body)
-    
-    blog
-        .save()
-        .then(result => {
-            res.status(201).json(result)
-        })
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
 })
 
 const PORT = 3003
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
