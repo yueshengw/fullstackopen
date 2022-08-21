01bebe4f0d837edabf4e068a41b79fddb9da557a
yueshengw <78110664+yueshengw@users.noreply.github.com>
2022-08-21 13:11:51 -0400

    Exercise 4.7 Complete

--- part4/blogs-backend/utils/list_helper.js
+++ part4/blogs-backend/utils/list_helper.js
@@ -25,6 +25,54 @@ const favoriteBlog = (blogs) => {
    }
 }
 
+const mostBlog = (blogs) => {
+    let authorList = []
+    let tempList = []
+    if (blogs && blogs.length > 0) {
+        authorList = blogs.map(blog => blog.author)
+        console.log(authorList)
+        authorList.forEach(author1 => {
+            let count = authorList.filter(author2 => author1 === author2).length
+            tempList.push({
+                author: author1,
+                blogs: count
+            })
+        })
+        return tempList.find(entry => entry.blogs === Math.max(...tempList.map(entry => entry.blogs)))
+    }
+    else {
+        return null
+    }
+}
+
+const mostLikes = (blogs) => {
+    let authorListUO = []
+    let authorList = []
+    let tempList = []
+    if (blogs && blogs.length > 0) {
+        authorListUO = blogs.map(blog => blog.author)
+        console.log(authorListUO)
+        authorListUO.forEach(author => {
+            let tempSum = 0
+            console.log(blogs.filter(entry => entry.author === author))
+            blogs.filter(entry => entry.author === author).forEach(entry => tempSum += entry.likes)
+            tempList.push({
+                author: author,
+                likes: tempSum
+            })
+        })
+        console.log(tempList)
+        return tempList.find(entry => entry.likes === Math.max(...tempList.map(entry => entry.likes)))
+    }
+    else {
+        return null
+    }
+}
+
 module.exports = {
-    dummy, totalLikes, favoriteBlog
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlog,
+    mostLikes
 }
