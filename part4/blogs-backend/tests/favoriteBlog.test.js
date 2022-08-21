a59268e26c243b29871259756499f2571d8c2286
yueshengw <78110664+yueshengw@users.noreply.github.com>
2022-08-21 11:25:46 -0400

    Exercise 4.5 Complete

--- part4/blogs-backend/tests/favoriteBlog.test.js
+++ part4/blogs-backend/tests/favoriteBlog.test.js
@@ -1,19 +1,6 @@
-const listHelper = require('../utils/list_helper')
+let listHelper = require('../utils/list_helper')
 
-describe('total likes', () => {
-    const emptyList = []
-
-    const listWithOneBlog = [
-      {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
-      }
-    ]
-    
+describe('favoriteBlog', () => {
     const blogs = [
         {
           _id: "5a422a851b54a676234d17f7",
@@ -36,7 +23,7 @@ describe('total likes', () => {
           title: "Canonical string reduction",
           author: "Edsger W. Dijkstra",
           url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-          likes: 12,
+          likes: 9,
           __v: 0
         },
         {
@@ -63,18 +50,33 @@ describe('total likes', () => {
           likes: 2,
           __v: 0
         }  
-      ]
+    ]
 
-    test('of empty list is zero', () => {
-        expect(listHelper.totalLikes(emptyList)).toBe(0)
+    test('of empty list is null', () => {
+        expect(listHelper.favoriteBlog([])).toBe(null)
     })
 
-    test('when list has only one blog, equals the likes of that', () => {
-      const result = listHelper.totalLikes(listWithOneBlog)
-      expect(result).toBe(5)
+    test('when list has only one blog, favorite blog is that', () => {
+        expect(listHelper.favoriteBlog([{
+            _id: "5a422b891b54a676234d17fa",
+            title: "First class tests",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+            likes: 10,
+            __v: 0
+          }])).toEqual({
+            title: "First class tests",
+            author: 'Robert C. Martin',
+            likes: 10
+          })
     })
 
-    test('of a bigger list is calculated right', () => {
-        expect(listHelper.totalLikes(blogs)).toBe(36)
+    test('of a bigger list is returned right', () => {
+        expect(listHelper.favoriteBlog(blogs)).toEqual({
+            title: "First class tests",
+            author: 'Robert C. Martin',
+            likes: 10
+          })
     })
 })
+
--- part4/blogs-backend/utils/list_helper.js
+++ part4/blogs-backend/utils/list_helper.js
@@ -4,12 +4,27 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
     let totalLikes = 0
-    blogs.forEach(blog => 
-        totalLikes += blog.likes
+    blogs.forEach(blog => Number.isFinite(blog.likes) ? totalLikes += blog.likes : null
     )
     return totalLikes
 }
 
+const favoriteBlog = (blogs) => {
+    let favoriteBlogIndex = 0
+    if (blogs && blogs.length > 0) {
+        favoriteBlogIndex = blogs.map(blog => blog.likes).indexOf(Math.max(...blogs.map(blog => blog.likes)))
+    }
+    else {
+        return null
+    }
+    
+   return favoriteBlog && {
+    title: blogs[favoriteBlogIndex].title,
+    author: blogs[favoriteBlogIndex].author,
+    likes: blogs[favoriteBlogIndex].likes
+   }
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes, favoriteBlog
 }
